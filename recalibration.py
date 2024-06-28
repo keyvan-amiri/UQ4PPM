fix data leakage in recalibration

recalibration.py
@@ -1,60 +1,228 @@
 import argparse
 import os
+import yaml
+import pickle
 import pandas as pd
+from datetime import datetime
+import torch
+from torch.utils.data import TensorDataset, DataLoader
 import uncertainty_toolbox as uct
 from utils.eval_cal_utils import get_csv_files
+from utils.eval_cal_utils import (extract_info_from_cfg, replace_suffix,
+                                  get_validation_data_and_model_size,
+                                  get_model_and_loss, get_uq_method,
+                                  add_suffix_to_csv)
 
+
+def calibration_on_validation(args=None, model=None, checkpoint_path=None,
+                              calibration_loader=None, heteroscedastic=None,
+                              num_mc_samples=None, normalization=False, 
+                              y_scaler=None, device=None, result_path=None):
+        
+    print('Now: start recalibration:')
+    start=datetime.now()
+    
+    # setstructure of instance-level results
+    if (args.UQ == 'DA' or args.UQ == 'CDA'):
+        res_dict = {'GroundTruth': [], 'Prediction': [],
+                    'Epistemic_Uncertainty': []}
+    elif (args.UQ == 'DA_A' or args.UQ == 'CDA_A'):
+        res_dict = {'GroundTruth': [], 'Prediction': [], 
+                    'Epistemic_Uncertainty': [], 'Aleatoric_Uncertainty': [],
+                    'Total_Uncertainty': []} 
+    elif args.UQ == 'mve':
+        res_dict = {'GroundTruth': [], 'Prediction': [],
+                    'Aleatoric_Uncertainty': []}
+    
+    # load the checkpoint  
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    
+    # get instance-level results on validation set
+    model.eval()
+    with torch.no_grad():
+        for index, calibration_batch in enumerate(calibration_loader):
+            # get batch data
+            inputs = calibration_batch[0].to(device)
+            _y_truth = calibration_batch[1].to(device)  
+            # get model outputs, and uncertainties
+            if (args.UQ == 'DA' or args.UQ == 'CDA' or args.UQ == 'DA_A' or
+                args.UQ == 'CDA_A'):
+                means_list, logvar_list =[], []
+                # conduct Monte Carlo sampling
+                for i in range (num_mc_samples): 
+                    mean, log_var,_ = model(inputs, stop_dropout=False)
+                    means_list.append(mean)
+                    logvar_list.append(log_var)
+                # Aggregate the results for all samples
+                # Compute point estimation and uncertainty
+                stacked_means = torch.stack(means_list, dim=0)
+                # predited value is the average for all samples
+                _y_pred = torch.mean(stacked_means, dim=0)
+                # epistemic uncertainty obtained from std for all samples
+                epistemic_std = torch.std(stacked_means, dim=0).to(device)
+                # normalize epistemic uncertainty if necessary
+                if normalization:
+                    epistemic_std = y_scaler * epistemic_std
+                # now obtain aleatoric uncertainty
+                if heteroscedastic:
+                    stacked_log_var = torch.stack(logvar_list, dim=0)
+                    stacked_var = torch.exp(stacked_log_var)
+                    mean_var = torch.mean(stacked_var, dim=0)
+                    aleatoric_std = torch.sqrt(mean_var).to(device)
+                    # normalize aleatoric uncertainty if necessary
+                    if normalization:
+                        aleatoric_std = y_scaler * aleatoric_std
+                    total_std = epistemic_std + aleatoric_std
+            elif args.UQ == 'mve':
+                _y_pred, log_var = model(inputs)
+                aleatoric_std = torch.sqrt(torch.exp(log_var))
+                # normalize aleatoric uncertainty if necessary
+                if normalization:
+                    aleatoric_std = y_scaler * aleatoric_std            
+            # convert tragets, outputs in case of normalization
+            if normalization:
+                _y_truth = y_scaler * _y_truth
+                _y_pred = y_scaler * _y_pred
+            # Detach predictions and ground truths (np arrays)
+            _y_truth = _y_truth.detach().cpu().numpy()
+            _y_pred = _y_pred.detach().cpu().numpy()
+            # collect inference result in all_result dict.
+            res_dict['GroundTruth'].extend(_y_truth.tolist())
+            res_dict['Prediction'].extend(_y_pred.tolist())
+            if (args.UQ == 'DA' or args.UQ == 'CDA' or args.UQ == 'DA_A' or 
+                args.UQ == 'CDA_A'):
+                epistemic_std = epistemic_std.detach().cpu().numpy()
+                res_dict['Epistemic_Uncertainty'].extend(epistemic_std.tolist()) 
+                if heteroscedastic:
+                    aleatoric_std = aleatoric_std.detach().cpu().numpy()
+                    total_std = total_std.detach().cpu().numpy()
+                    res_dict['Aleatoric_Uncertainty'].extend(aleatoric_std.tolist())
+                    res_dict['Total_Uncertainty'].extend(total_std.tolist()) 
+            elif args.UQ == 'mve':
+                aleatoric_std = aleatoric_std.detach().cpu().numpy()
+                res_dict['Aleatoric_Uncertainty'].extend(aleatoric_std.tolist())
+    calibration_df = pd.DataFrame(res_dict)
+    pred_mean, pred_std, y_true = get_mean_std_truth(
+        calibration_df=calibration_df, uq_method=args.UQ)
+    if args.method == 'std_ratio':
+        recalibrator = uct.recalibration.get_std_recalibrator(
+            pred_mean, pred_std, y_true, criterion=args.criterion)
+        # Apply the recalibrator to get recalibrated standard deviations
+        recalibrated_std = recalibrator(pred_std)
+        calibration_df['calibrated_std'] = recalibrated_std 
+    else:
+        # TODO: implement any other recalibration approach
+        pass
+    calibration_time = (datetime.now()-start).total_seconds()
+    new_csv_name = add_suffix_to_csv(args.csv_file,
+                                     added_suffix='validation_calibrated_')
+    new_csv_path = os.path.join(result_path, new_csv_name)
+    calibration_df.to_csv(new_csv_path, index=False)
+
+
+def get_mean_std_truth (calibration_df=None, uq_method=None):
+    pred_mean = calibration_df['Prediction'].values 
+    y_true = calibration_df['GroundTruth'].values
+    if (uq_method=='DA_A' or uq_method=='CDA_A'):
+        pred_std = calibration_df['Total_Uncertainty'].values 
+    elif (uq_method=='CARD' or uq_method=='mve'):
+        pred_std = calibration_df['Aleatoric_Uncertainty'].values
+    elif (uq_method=='DA' or uq_method=='CDA'):
+        pred_std = calibration_df['Epistemic_Uncertainty'].values
+    else:
+        raise NotImplementedError(
+            'Uncertainty quantification {} not understood.'.format(uq_method))
+    return pred_mean, pred_std, y_true
 
 def main():   
     
     # Parse arguments 
     parser = argparse.ArgumentParser(
         description='Recalibration for specified models')
-    parser.add_argument('--dataset', default='HelpDesk',
-                        help='Datasets that is used for recalibration')
-    parser.add_argument('--model', default='dalstm',
-                        help='Type of the predictive model')
+    parser.add_argument('--device', type=int, default=0, help='GPU device id')
+    parser.add_argument('--csv_file', help='results to be recalibrated')
+    parser.add_argument('--cfg_file', help='configuration used for training')
+    parser.add_argument('--method', default='std_ratio',
+                        help='recalibration criterion to be used')
     parser.add_argument('--criterion', default='miscal',
-                        help='miscalibration criterion to be used')
+                        help='recalibration criterion to be used')
     args = parser.parse_args()
     
     # Ensure criterion is one of the allowed values
     allowed_criteria = {'ma_cal', 'rms_cal', 'miscal'}
-    assert args.criterion in allowed_criteria
-    
-    # define model directory for recalibration
+    assert args.criterion in allowed_criteria  
+    # Define the device
+    device_name = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(device_name)
+    # Define path for cfg and csv files
     root_path = os.getcwd()
-    model_path = os.path.join(root_path, 'results', args.dataset, args.model)
+    cfg_path = os.path.join(root_path, 'cfg', args.cfg_file)
     
-    # find all csv files created by predictive model
-    csv_files, uncertainty_methods = get_csv_files(model_path)
-    
-    # iterate over results, and recalibrate predictions
-    for i in range (len(csv_files)):
-        # Extract the file name and extension
-        base_name, ext = os.path.splitext(os.path.basename(csv_files[i]))
-        # read csv file, and find the uncertainty quantification method used
-        df = pd.read_csv(csv_files[i])
-        prefix = uncertainty_methods[i]
-        # get ground truth as well as mean and std for predictions
-        pred_mean = df['Prediction'].values 
-        y_true = df['GroundTruth'].values
-        if (prefix=='DA_A' or prefix=='CDA_A'):
-            pred_std = df['Total_Uncertainty'].values 
-        elif (prefix=='CARD' or prefix=='mve'):
-            pred_std = df['Aleatoric_Uncertainty'].values
-        elif (prefix=='DA' or prefix=='CDA'):
-            pred_std = df['Epistemic_Uncertainty'].values
-        else:
-            raise NotImplementedError(
-                'Uncertainty quantification {} not understood.'.format(prefix))
-            
-        recalibrator = uct.recalibration.get_std_recalibrator(
-            pred_mean, pred_std, y_true, criterion=args.criterion)
-        # Apply the recalibrator to get recalibrated standard deviations
-        recalibrated_std = recalibrator(pred_std)
-
+    # Get model, dataset, and uq_method based on configuration file name
+    args.model, args.dataset, args.UQ = extract_info_from_cfg(args.cfg_file)    
+    result_path = os.path.join(root_path, 'results', args.dataset, args.model)
+    csv_path = os.path.join(result_path, args.csv_file)
+    if args.UQ != 'CARD':
+        args.UQ = get_uq_method(args.csv_file)
+        # load cfg file used for training
+        with open(cfg_path, 'r') as f:
+            cfg = yaml.safe_load(f) 
+        # define path for report .txt to add recalibration time
+        report_name = replace_suffix(args.csv_file, 'inference_result_.csv',
+                                     'report_.txt')
+        report_path = os.path.join(root_path, 'results', args.dataset,
+                                   args.model, report_name)
+        # define name of the check point (best model)
+        checkpoint_name = replace_suffix(args.csv_file, 'inference_result_.csv',
+                                         'best_model.pt')
+        checkpoint_path = os.path.join(result_path, checkpoint_name)
         
+    else:
+        # TODO: access the relevant configuration file
+        # TODO: access the model checkpoint
+        # TODO: handle report .txt for time!
+        pass
+           
+    # TODO: adjust when it is done in the past
+    calibration_history = False
+    
+    if not calibration_history:
+        # Separate execution path for CARD approach   
+        if args.UQ == 'CARD':
+            pass
+        else:
+            # Get calibration loader, model dimensions, normalization ratios
+            (calibration_loader, input_size, max_len, max_train_val,
+             mean_train_val, median_train_val
+             ) = get_validation_data_and_model_size(args=args, cfg=cfg,
+                                                    root_path=root_path)
+            # define model and loss function
+            (model, criterion, heteroscedastic, num_mcmc, normalization
+             ) = get_model_and_loss(args=args, cfg=cfg, input_size=input_size,
+                                    max_len=max_len, device=device)
+            # execute calibration on validation set
+            calibration_on_validation(
+                args=args, model=model, checkpoint_path=checkpoint_path,
+                calibration_loader=calibration_loader,
+                heteroscedastic=heteroscedastic, num_mc_samples=num_mcmc,
+                normalization=normalization, y_scaler=mean_train_val,
+                device=device, result_path=result_path)
+
+            
+        
+            
+ 
+                
+
+
+    
+    """
+   
+    # get a dataframe containint test results to be recalibrated    
+    test_df_path = os.path.join(root_path, 'results', args.csv_file)
+    test_df = pd.read_csv(test_df_path)
+    
         if (prefix=='DA_A' or prefix=='CDA_A'):
             pred_std = df['Total_Uncertainty'] = recalibrated_std
         elif (prefix=='CARD' or prefix=='mve'):
@@ -64,6 +232,7 @@ def main():
         
         new_csv_path = os.path.join(model_path, base_name + 'recalibrated_.csv')
         df.to_csv(new_csv_path, index=False)
+        """
         
 if __name__ == '__main__':
     main()        

utils/eval_cal_utils.py
@@ -1,14 +1,22 @@
 import glob
 import re
 import os
+import pickle
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from models.dalstm import DALSTMModelMve
+from models.stochastic_dalstm import StochasticDALSTM
+from loss.loss_handler import set_loss
 
 # A mothod to retrieve all results for a combination of dataset-model
 def get_csv_files(folder_path):
     # Get all csv files in the folder
     all_csv_files = glob.glob(os.path.join(folder_path, '*.csv'))    
     # Filter out files containing 'deterministic' in their names
-    filtered_csv_files = [f for f in all_csv_files if 
+    filtered_csvs = [f for f in all_csv_files if 
                           'deterministic' not in os.path.basename(f).lower()]
+    filtered_csv_files = [f for f in filtered_csvs if 
+                          'validation_calibrated' not in os.path.basename(f).lower()]
     # Collect name of uncertainty quantification approaches
     prefixes = []
     pattern = re.compile(r'(.*?)(_holdout_|_cv_)')
@@ -18,3 +26,176 @@ def get_csv_files(folder_path):
         if match:
             prefixes.append(match.group(1))   
     return filtered_csv_files, prefixes
+
+
+def extract_info_from_cfg(cfg_file):
+    # Define the regex pattern
+    pattern = r"^(?P<model>[a-zA-Z0-9]+)_(?P<dataset>[a-zA-Z0-9]+)(?:_(?P<uq_method>[a-zA-Z0-9]+))?\.(?:yaml|yml)$"    
+    # Match the pattern against the cfg_file
+    match = re.match(pattern, cfg_file)    
+    if match:
+        model = match.group('model')
+        dataset = match.group('dataset')
+        uq_method = match.group('uq_method')  
+        if uq_method!=None:
+            uq_method=uq_method.upper()
+        return model, dataset, uq_method
+    else:
+        raise ValueError('The configuration file name does not match the expected pattern.')
+        
+def get_uq_method(csv_file):
+    # Define the regex pattern to capture uq_method
+    pattern = r"^(?P<uq_method>.+?)_(holdout|cv)_.*\.csv$"
+    
+    # Match the pattern against the csv_file
+    match = re.match(pattern, csv_file)
+    
+    if match:
+        uq_method = match.group('uq_method')
+        return uq_method
+    else:
+        raise ValueError("The CSV file name does not match the expected pattern.")
+        
+def replace_suffix(input_string, old_suffix, new_suffix):
+    if input_string.endswith(old_suffix):
+        return input_string[:-len(old_suffix)] + new_suffix
+    else:
+        raise ValueError(
+            'The input string does not end with the specified old suffix.')
+        
+def extract_fold(csv_file):
+    # Define the regex pattern with fold
+    pattern_with_fold = r"^[^_]+_[^_]+_cv_fold(?P<fold>\d+)_seed_\d+_inference_result_\.csv$"
+    
+    # Match the pattern against the csv_file
+    match = re.match(pattern_with_fold, csv_file)
+    
+    if match:
+        fold = match.group('fold')
+        return fold
+    else:
+        return None
+
+def add_suffix_to_csv(csv_file, added_suffix=None):
+    # Check if the file name ends with .csv
+    if csv_file.endswith('.csv'):
+        # Insert the suffix before the .csv extension
+        new_csv_file = csv_file[:-4] + added_suffix + '.csv'
+        return new_csv_file
+    else:
+        raise ValueError("The file name does not end with .csv")
+    
+def get_validation_data_and_model_size(args=None, cfg=None, root_path=None):
+    # check holdout/cv data split and get the fold
+    fold = extract_fold(args.csv_file)    
+    if args.model == 'dalstm':
+        dataset_class = 'DALSTM_'+ args.dataset
+        daaset_path = os.path.join(root_path, 'datasets', dataset_class)
+        if fold == None:
+            X_val_path = os.path.join(
+                daaset_path, 'DALSTM_X_val_'+args.dataset+'.pt')
+            y_val_path = os.path.join(
+                daaset_path, 'DALSTM_y_val_'+args.dataset+'.pt')
+        else:
+            X_val_path = os.path.join(
+                daaset_path, 'DALSTM_X_val_fold_'+fold+args.dataset+'.pt')
+            y_val_path = os.path.join(
+                daaset_path, 'DALSTM_y_val_fold_'+fold+args.dataset+'.pt')
+        # load validation data
+        X_val = torch.load(X_val_path)
+        y_val = torch.load(y_val_path)
+        # load model's dimensions
+        input_size_path = os.path.join(
+            daaset_path, 'DALSTM_input_size_'+args.dataset+'.pkl')
+        max_len_path = os.path.join(
+            daaset_path, 'DALSTM_max_len_'+args.dataset+'.pkl')
+        max_train_val_path = os.path.join(
+            daaset_path, 'DALSTM_max_train_val_'+args.dataset+'.pkl')
+        mean_train_val_path = os.path.join(
+            daaset_path, 'DALSTM_mean_train_val_'+args.dataset+'.pkl')
+        median_train_val_path = os.path.join(
+            daaset_path, 'DALSTM_median_train_val_'+args.dataset+'.pkl')
+        with open(input_size_path, 'rb') as f:
+            input_size =  pickle.load(f)
+        with open(max_len_path, 'rb') as f:
+            max_len =  pickle.load(f) 
+        with open(max_train_val_path, 'rb') as f:
+            max_train_val =  pickle.load(f) 
+        with open(mean_train_val_path, 'rb') as f:
+            mean_train_val =  pickle.load(f)
+        with open(median_train_val_path, 'rb') as f:
+            median_train_val =  pickle.load(f)
+        # get relevant batch size
+        try:
+            calibration_batch_size = cfg.get('evaluation').get('batch_size')
+        except:
+            calibration_batch_size = max_len 
+        # create validation set
+        calibration_dataset = TensorDataset(X_val, y_val)
+        calibration_loader = DataLoader(calibration_dataset,
+                                        batch_size=calibration_batch_size,
+                                        shuffle=False)
+        
+        return (calibration_loader, input_size, max_len, max_train_val,
+                mean_train_val, median_train_val)
+     
+    #TODO: to complete this part for other architectures
+    elif args.model == 'pgtnet':
+        dataset_class = 'PGTNet_'+ args.dataset
+
+        return None
+
+
+def get_model_and_loss(args=None, cfg=None, input_size=None, max_len=None, 
+                       device=None):
+
+    # get other model characteristics
+    hidden_size = cfg.get('model').get('lstm').get('hidden_size')
+    n_layers = cfg.get('model').get('lstm').get('n_layers')
+    dropout = cfg.get('model').get('lstm').get('dropout')  
+    dropout_prob = cfg.get('model').get('lstm').get('dropout_prob')
+    normalization = cfg.get('data').get('normalization')
+    
+    # define model, loss function, and other important variables                            
+    if args.UQ == 'mve':
+        model = DALSTMModelMve(input_size=input_size, hidden_size=hidden_size,
+                               n_layers=n_layers, max_len=max_len,
+                               dropout=dropout, p_fix=dropout_prob).to(device)
+        # use heteroscedastic loss funciton for MVE approach
+        criterion = set_loss(loss_func=cfg.get('train').get('loss_function'),
+                             heteroscedastic=True)  
+        heteroscedastic = False 
+        num_mcmc = None
+    elif (args.UQ == 'DA' or args.UQ == 'CDA' or args.UQ == 'DA_A' or 
+          args.UQ == 'CDA_A'):
+        dropout = True
+        Bayes = True
+        num_mcmc = cfg.get('uncertainty').get('dropout_approximation').get(
+            'num_stochastic_forward_path')
+        weight_regularizer = cfg.get('uncertainty').get(
+            'dropout_approximation').get('weight_regularizer')
+        dropout_regularizer = cfg.get('uncertainty').get(
+            'dropout_approximation').get('dropout_regularizer')
+        # Set the parameter for concrete dropout
+        if (args.UQ == 'DA' or args.UQ == 'DA_A'):
+            concrete_dropout = False
+        else:
+            concrete_dropout = True
+        # Set the loss function (heteroscedastic/homoscedastic)
+        if (args.UQ == 'DA' or args.UQ == 'CDA'):
+            heteroscedastic = False
+            criterion = set_loss(loss_func=cfg.get('train').get('loss_function'))
+        else:
+            heteroscedastic = True
+            criterion = set_loss(loss_func=cfg.get('train').get('loss_function'),
+                                 heteroscedastic=True) 
+            model = StochasticDALSTM(input_size=input_size, hidden_size=hidden_size,
+                                     n_layers=n_layers, max_len=max_len,
+                                     dropout=dropout, concrete=concrete_dropout,
+                                     p_fix=dropout_prob, 
+                                     weight_regularizer=weight_regularizer,
+                                     dropout_regularizer=dropout_regularizer,
+                                     hs=heteroscedastic, Bayes=Bayes, 
+                                     device=device).to(device)
+            
+    return (model, criterion, heteroscedastic, num_mcmc, normalization)

utils/utils.py
@@ -192,17 +192,20 @@ def test_model(model=None, uq_method=None, heteroscedastic=None,
                test_original_lengths=None, y_scaler=None, 
                processed_data_path=None, report_path=None,
                data_split=None, fold=None, seed=None, device=None,
-               normalization=False):
-    print('Now: start inference- Holdout data split:')
+               normalization=False): 
+    
     start=datetime.now()
     if data_split=='holdout':
+        print(f'Now: start inference- data split: {data_split}.')
         checkpoint_path = os.path.join(processed_data_path,
                                        '{}_{}_seed_{}_best_model.pt'.format(
                                            uq_method, data_split, seed)) 
     else:
+        print(f'Now: start inference- data split: {data_split} ,  fold: {fold}.')
         checkpoint_path = os.path.join(
             processed_data_path, '{}_{}_fold{}_seed_{}_best_model.pt'.format(
-                uq_method, data_split, fold, seed))        
+                uq_method, data_split, fold, seed)) 
+        
     checkpoint = torch.load(checkpoint_path)
     model.load_state_dict(checkpoint['model_state_dict'])
     # define the columns in csv file for the results