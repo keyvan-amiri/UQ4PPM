fedc8c42be385d24731539ac40c7307ee4d2a490
keyvan-amiri <kamiriel@uni-mannheim.de>
2024-08-30 16:47:25 +0200

    fix a serialization bug

--- models/Laplace_approximation.py
+++ models/Laplace_approximation.py
@@ -24,6 +24,7 @@ from loss.mape import mape
 from laplace import Laplace
 from utils.eval_cal_utils import (get_validation_data_and_model_size,
                                   replace_suffix)
+from models.dalstm import DALSTMModel
 
 
 def post_hoc_laplace(model=None, cfg=None,
@@ -241,8 +242,10 @@ def post_hoc_laplace(model=None, cfg=None,
                     'Epoch {}/{} NLL: {}, log prior: {}, log sigma: {}.\n'.format(
                         i+1, la_epochs, neg_marglik, log_prior, log_sigma))     
         
-    #save the Laplace model (including deterministic model wrapped in it)
-    torch.save(la, la_path, pickle_module=dill)
+    #save the Laplace model
+    torch.save(la.state_dict(), la_path)
+    # initial implementation: include backbone in the object!
+    #torch.save(la, la_path, pickle_module=dill)
     
     training_time = (datetime.now()-start).total_seconds()
     print('Fitting and optimizing the Laplace approximation is done')
@@ -368,16 +371,60 @@ def inf_val_laplace(args=None, cfg=None, device=None,
     start=datetime.now()   
     # get normalization mode
     normalization = cfg.get('data').get('normalization') 
+    # get important model dimension, and hyper-parameters
+    n_layers = cfg.get('model').get('lstm').get('n_layers')
+    hidden_size = cfg.get('model').get('lstm').get('hidden_size')
+    dropout = cfg.get('model').get('lstm').get('dropout')
+    dropout_prob = cfg.get('model').get('lstm').get('dropout_prob')
     pred_type = 'glm' # define prediction type
+    subset_of_weights= 'last_layer' # define name of last layer in Backbone
+    last_layer_name = cfg.get('uncertainty').get('laplace').get(
+        'last_layer_name')
+    hessian_structure = cfg.get('uncertainty').get(
+        'laplace').get('hessian_structure')
+    sigma_noise = cfg.get('uncertainty').get('laplace').get(
+        'sigma_noise')
+    prior_precision = cfg.get('uncertainty').get('laplace').get(
+        'prior_precision')
+    temperature= cfg.get('uncertainty').get('laplace').get(
+        'temperature')
     
+    # get calibration loader (i.e., validation loader), and some others!
     (calibration_loader, input_size, max_len, y_scaler, _, _
      ) = get_validation_data_and_model_size(args=args, cfg=cfg, 
                                             root_path=root_path)
-    # load fitted and optimized Laplace model
+    # set name and path for checkpoints: Laplace model + Backbone point estimate
     laplace_model_name = replace_suffix(
         args.csv_file, 'inference_result_.csv', 'best_model.pt')
+    parts = laplace_model_name.split('_', 1)
+    deterministic_checkpoint_name = 'deterministic_' + parts[1]
+    deterministic_checkpoint_path = os.path.join(result_path,
+                                                 deterministic_checkpoint_name)
+    # define the Backbone model, and load its state_dict
+    model = DALSTMModel(
+        input_size=input_size,
+        hidden_size=hidden_size,
+        n_layers=n_layers,
+        max_len=max_len,
+        dropout=dropout,
+        p_fix=dropout_prob,
+        return_squeezed=False).to(device)
+    model.load_state_dict(torch.load(deterministic_checkpoint_path))
+    
+    # load fitted and optimized Laplace model
+    optional_args = dict() #empty dict for optional args in Laplace model
+    optional_args['last_layer_name'] = last_layer_name 
+    la = Laplace(model, likelihood='regression',
+                 subset_of_weights=subset_of_weights,
+                 hessian_structure=hessian_structure,
+                 sigma_noise=sigma_noise,
+                 prior_precision=prior_precision,
+                 temperature=temperature,
+                 **optional_args) 
+    
     laplace_model_path = os.path.join(result_path, laplace_model_name)
-    la = torch.load(laplace_model_path)
+    la.load_state_dict(torch.load(laplace_model_path))
+
     # empty dict to collct predictios on validation (i.e., calibration) set
     res_dict = {'GroundTruth': [], 'Prediction': [], 'Epistemic_Uncertainty': []}
     
