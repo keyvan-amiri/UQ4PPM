bd393592158d59ad95308af1cabe46fda91918a8
keyvan-amiri <kamiriel@uni-mannheim.de>
2024-09-03 10:37:51 +0200

    Update preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,7 @@
 import warnings
 import argparse
 from utils.utils import str2bool
-#from utils.PGTNet_convertor import PGTNet_convertor_case_centric
+from utils.PGTNet_convertor import PGTNet_convertor_case_centric
 from utils.DALSTM_processing import DALSTM_preprocessing
 
 def main():    
@@ -13,6 +13,8 @@ def main():
     # can handle multiple event logs at the same time
     parser.add_argument('--datasets', nargs='+',
                         help='Raw datasets to be pre-processed', required=True)
+    parser.add_argument('--model', default='pgtnet',
+                        help='Type of the predictive model')
     parser.add_argument('--seed', type=int, default=42,
                         help='Number of splits that is used')
     # DALSTM don't apply any normalization to remaining time (target attribute)
@@ -22,29 +24,32 @@ def main():
     # PGTNet apply min-max normalization to remaining time (target attribute)  
     parser.add_argument('--normalization_pgtnet', type=str2bool, nargs='?',
                         const=True, default=True,
-                        help='Whether to apply normalization for dalstm.')
+                        help='Whether to apply normalization for PGTNet.')
     
     args = parser.parse_args()
     dataset_names = args.datasets
     #print(dataset_names)
     for dataset in dataset_names:
-        
+        print(f'Pre-processing is conducted for {dataset} dataset, \
+                  and {args.model} model')
         # data preprocessing for DALSTM approach
-        if args.normalization_lstm:
-            DALSTM_preprocessing(dataset_name=dataset, seed=args.seed, 
-                                 normalization=args.normalization_lstm)
-        else:
-            DALSTM_preprocessing(dataset_name=dataset, seed=args.seed)
-            
-        """
+        if args.model == 'dalstm':            
+            if args.normalization_lstm:
+                DALSTM_preprocessing(dataset_name=dataset, seed=args.seed,
+                                     normalization=args.normalization_lstm)
+            else:
+                DALSTM_preprocessing(dataset_name=dataset, seed=args.seed)
         # create graph dataset representation of the event log for PGTNet
-        # if normalization is included in command line arguments:
-        if args.normalization_pgtnet:                   
-            PGTNet_convertor_case_centric(dataset_name=dataset)
-        else:           
-            PGTNet_convertor_case_centric(
-                dataset_name=dataset, normalization=args.normalization_pgtnet)
-        """
+        elif args.model == 'pgtnet':      
+            # if normalization is included in command line arguments:
+            if args.normalization_pgtnet:                   
+                PGTNet_convertor_case_centric(dataset_name=dataset)
+            else:           
+                PGTNet_convertor_case_centric(
+                    dataset_name=dataset,
+                    normalization=args.normalization_pgtnet)
+        else:
+            print(f'{args.model} architecture is currently not supported.')
     
 if __name__ == '__main__':
     main()
